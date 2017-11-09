df93506f1c380b05f46c08a59594f41990248fc6
Miguel Angel Maffet Wall <miguelangelmaffet@gmail.com>
2017-11-09 03:53:24 -0300

    adss

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from json import dumps
 from flask.ext.jsonpify import jsonify
 from flask.ext.cors import CORS
-import threading
+from thread import start_new_thread
 
 import estado
 import sensor
